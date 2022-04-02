2300969a5f743e44f260b61167bbd0682887cad0
peugeot <thomaspeugeot@yahoo.fr>
2022-04-02 04:15:10 +0200

    integration of ng2-chart

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # gongngxcharts
+
+## install dep
+
+https://valor-software.com/ng2-charts/next/
+
+```bash
+npm install --save ng2-charts
+npm install --save chart.js
+```
+
+```ts
+    import { NgChartsModule } from 'ng2-charts';
+
+    // In your App's module:
+    imports: [
+      NgChartsModule
+    ]
+```
--- ng/projects/gongngxchartsspecific/src/lib/gongngxchartsspecific.module.ts
+++ ng/projects/gongngxchartsspecific/src/lib/gongngxchartsspecific.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { GongngxchartsspecificComponent } from './gongngxchartsspecific.component';
 import { GongngxchartsChartComponent } from './gongngxcharts-chart/gongngxcharts-chart.component';
 
-
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -10,6 +10,7 @@ import { GongngxchartsChartComponent } from './gongngxcharts-chart/gongngxcharts
     GongngxchartsChartComponent
   ],
   imports: [
+    NgChartsModule,
   ],
   exports: [
     GongngxchartsspecificComponent,
